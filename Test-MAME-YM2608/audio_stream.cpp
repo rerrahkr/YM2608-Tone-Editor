cf26dfa43281ccbc3abefade2787452f5278bf83
rerrahkr <rkr.5.tecknzi@gmail.com>
2018-03-16 17:55:16 +0900

    Fix choppy streaming audio

--- Test-MAME-YM2608/audio_stream.cpp
+++ Test-MAME-YM2608/audio_stream.cpp
@@ -1,18 +1,18 @@
 #include "audio_stream.hpp"
 #include <QSysInfo>
 
-AudioStream::AudioStream(chip::OPNA& chip, uint32 duration) :
+AudioStream::AudioStream(chip::OPNA& chip, uint32 rate, uint32 duration) :
     chip_(chip)
 {
     format_.setByteOrder(QAudioFormat::Endian(QSysInfo::ByteOrder));
     format_.setChannelCount(2); // Stereo
     format_.setCodec("audio/pcm");
-    format_.setSampleRate(chip_.getRate());
+    format_.setSampleRate(rate);
     format_.setSampleSize(16);   // int16
     format_.setSampleType(QAudioFormat::SignedInt);
 
     audio_ = std::make_unique<QAudioOutput>(format_);
-    mixer_ = std::make_unique<AudioStreamMixier>(chip_, duration);
+    mixer_ = std::make_unique<AudioStreamMixier>(chip_, rate, duration);
     start();
 }
 
@@ -26,13 +26,14 @@ void AudioStream::setRate(uint32 rate)
     stop();
     format_.setSampleRate(rate);
     audio_ = std::make_unique<QAudioOutput>(format_);
+    mixer_->setRate(rate);
     start();
 }
 
 void AudioStream::setDuration(uint32 duration)
 {
     stop();
-    mixer_ = std::make_unique<AudioStreamMixier>(chip_, duration);
+    mixer_->setDuration(duration);
     start();
 }
 
