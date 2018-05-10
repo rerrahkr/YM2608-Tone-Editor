c9f5fbd7569f690ada65604385c441110820ee4f
rerrahkr <rerrah57@gmail.com>
2018-05-10 15:29:49 +0900

    Fix form design

--- Test-MAME-YM2608/aboutdialog.cpp
+++ Test-MAME-YM2608/aboutdialog.cpp
@@ -7,15 +7,9 @@ AboutDialog::AboutDialog(QWidget *parent) :
 {
 	ui->setupUi(this);
 
-	setFixedSize(size());
-	setWindowFlag(Qt::MSWindowsFixedSizeDialogHint);
-	setWindowFlag(Qt::WindowContextHelpButtonHint, false);
-
-	ui->label->setText(
-				"YM2608 Tone Editor\n"
-				"Version 0.1.0\n"
-				"Copyright © 2018 Rerrah"
-				);
+    setFixedSize(size());
+    setWindowFlag(Qt::MSWindowsFixedSizeDialogHint);
+    setWindowFlag(Qt::WindowContextHelpButtonHint, false);
 }
 
 AboutDialog::~AboutDialog()
