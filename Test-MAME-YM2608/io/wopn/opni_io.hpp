412b4c9dfa62b5de50ca9fd2983d21ba696ca630
rerrahkr <rerrah57@gmail.com>
2020-08-16 20:40:56 +0900

    Support .opni export

--- Test-MAME-YM2608/io/wopn/opni_io.hpp
+++ Test-MAME-YM2608/io/wopn/opni_io.hpp
@@ -7,4 +7,5 @@ class OpniIo final : public AbstractSingleToneIo
 public:
 	OpniIo();
 	Tone* load(const BinaryContainer& container) const override;
+	const BinaryContainer save(const Tone& tone) const override;
 };
