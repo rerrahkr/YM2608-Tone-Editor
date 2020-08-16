62128d02a4de1549e1f319a39a6d8d60d59b8687
rerrahkr <rerrah57@gmail.com>
2020-08-16 20:40:56 +0900

    Support .opni export

--- Test-MAME-YM2608/io/wopn/opni_io.cpp
+++ Test-MAME-YM2608/io/wopn/opni_io.cpp
@@ -1,8 +1,10 @@
 #include "opni_io.hpp"
+#include <cstring>
+#include <algorithm>
 #include "wopn_file.h"
 #include "../file_io_error.hpp"
 
-OpniIo::OpniIo() : AbstractSingleToneIo("opni", "OPN2BankEditor instrument", true, false) {}
+OpniIo::OpniIo() : AbstractSingleToneIo("opni", "OPN2BankEditor instrument", true, true) {}
 
 Tone* OpniIo::load(const BinaryContainer& container) const
 {
@@ -35,4 +37,42 @@ Tone* OpniIo::load(const BinaryContainer& container) const
 	}
 
 	return tone.release();
+}
+
+const BinaryContainer OpniIo::save(const Tone& tone) const
+{
+	BinaryContainer container(std::vector<char>(79));
+
+	OPNIFile opni;
+	opni.version = 2;
+	opni.is_drum = false;
+	WOPNInstrument& inst = opni.inst;
+
+	size_t nameLen = std::min<size_t>(tone.name.length(), sizeof(inst.inst_name) - 1);
+	std::strncpy(inst.inst_name, tone.name.c_str(), nameLen);
+	inst.inst_name[nameLen] = '\0';
+	inst.note_offset = 0;
+	inst.percussion_key_number = 0;
+	inst.lfosens = 0;
+	inst.inst_flags = WOPN_ChipType::WOPN_Chip_OPNA;
+
+	const int refOffs[] = { 0, 2, 1, 3 };
+	for (size_t o = 0; o < 4; ++o) {
+		WOPNOperator& op = inst.operators[o];
+		const Operator* refOp = tone.op + refOffs[o];
+		op.dtfm_30 = (refOp->DT << 4) | refOp->ML;
+		op.level_40 = refOp->TL;
+		op.rsatk_50 = (refOp->KS << 6) | refOp->AR;
+		op.decay2_70 = refOp->SR;
+		op.susrel_80 = (refOp->SL << 4) | refOp->RR;
+		op.amdecay1_60 = (refOp->AM << 7) | refOp->DR;
+		op.ssgeg_90 = refOp->SSGEG;
+	}
+	inst.fbalg = (tone.FB << 3) | tone.AL;
+
+	if (WOPN_SaveInstToMem(&opni, const_cast<char*>(container.getPointer()),
+						   static_cast<size_t>(container.size()), 2) != 0)
+		throw FileOutputError(FileIo::FileType::SingleTone);
+
+	return container;
 }
